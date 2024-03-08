25377a5e02d51bd31b7f42e1c181ae3486e31faf
dpinvar <141411725+dpinvar@users.noreply.github.com>
2024-03-08 11:05:44 +0000

    app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+// Display a simple message in the console
+console.log("Hello, GitHub!");
+
+// Perform a basic addition operation
+const num1 = 5;
+const num2 = 10;
+const sum = num1 + num2;
+console.log("The sum of", num1, "and", num2, "is", sum);
